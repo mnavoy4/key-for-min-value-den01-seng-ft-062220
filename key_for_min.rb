df5a29aac12eff7cc69348fc6101208a09e6b687
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-22 16:07:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   answer = nil
   name_hash.each do |name, value|
     if smallest_value == 0 || value < smallest_value
+      smallest_value = value
       answer = name
       
     end
